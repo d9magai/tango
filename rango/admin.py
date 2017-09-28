cd3fbfa2ba0c9d8d7dda403fd2b3293e1e5ab6db
d9magai <kumagai@sencorp.co.jp>
2017-09-29 07:24:10 +0900

    modify category

--- rango/admin.py
+++ rango/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 from rango.models import Category, Page
 
-admin.site.register(Category)
-admin.site.register(Page)
+# Add in this class to customized the Admin Interface
 
+
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+
+
+# Update the registeration to include this customised interface
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Page)
