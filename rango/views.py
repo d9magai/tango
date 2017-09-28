7fb535216094ef2b87718d16e7142a24a9902e2c
d9magai <kumagai@sencorp.co.jp>
2017-09-28 23:36:34 +0900

    show rango/index.html

--- rango/views.py
+++ rango/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
+
 
 def index(request):
-        return HttpResponse("Rango says hey there world!")
-
+    context_dict = {'boldmessage': "I am bold font from the context"}
+    return render(request, 'rango/index.html', context_dict)
--- tango/settings.py
+++ tango/settings.py
@@ -54,7 +54,9 @@ ROOT_URLCONF = 'tango.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            'templates'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
