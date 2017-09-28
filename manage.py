fe6cc3cd69b05bb78b964117d0f685492c36aba9
d9magai <kumagai@sencorp.co.jp>
2017-09-28 23:51:11 +0900

    setting databases

--- manage.py
+++ manage.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 import os
+import pymysql
 import sys
+pymysql.install_as_MySQLdb()
+
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tango.settings")
