f030f4f336fd7908dc3dff2a718487ac3269fd36
d9magai <kumagai@sencorp.co.jp>
2017-09-29 01:47:23 +0900

    show category

--- rango/views.py
+++ rango/views.py
@@ -1,7 +1,16 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from rango.models import Category
+
 
 def index(request):
-    context_dict = {'boldmessage': "I am bold font from the context"}
+    # Query the database for a list of ALL categories currently stored.
+    # Order the categories by no. likes in descending order.
+    # Retrieve the top 5 only - or all if less than 5.
+    # Place the list in our context_dict dictionary which will be passed to the template engine.
+    category_list = Category.objects.order_by('id')[:5]
+    context_dict = {'categories': category_list}
+
+    # Render the response and send it back!
     return render(request, 'rango/index.html', context_dict)
